e4215d40433f8468a96b77de79fad65b34d29927
Raymond <raymond.mak@hotmail.co.uk>
2024-02-05 20:55:30 +0000

    wasd

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,10 +2,10 @@ module.exports = {
 	title: "Eleventy Base Blog v8",
 	url: "https://example.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "This is the third time I am trying to test this blog",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
+		name: "Raymond M",
+		email: "raymond.mak@example.com",
 		url: "https://example.com/about-me/"
 	}
 }
---
+++ content/blog/sixthpost.md
@@ -0,0 +1,8 @@
+---
+title: What's up guys! New Post for 2024
+date: 2024-02-05
+draft: true
+tags :
+    - number 6
+---
+I am writing a blog 
